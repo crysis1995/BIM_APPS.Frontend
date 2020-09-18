01224cbba65911c5856f3632095242ad90d439b2
Krzysztof Kaczor <krzysztofkaczor95@gmail.com>
2020-09-18 06:42:52 +0200

    Update TermsComponent.js

--- src/sites/work_progress/components/Terms/TermsComponent.js
+++ src/sites/work_progress/components/Terms/TermsComponent.js
@@ -3,39 +3,49 @@ import { Accordion, Button, Table } from 'react-bootstrap';
 import { connect } from 'react-redux';
 
 function TermsComponent(props) {
+	const onClickHandler = (e) => {
+		const hiddenElement = e.currentTarget.nextSibling;
+		hiddenElement.className.indexOf('collapse show') > -1
+			? hiddenElement.classList.remove('show')
+			: hiddenElement.classList.add('show');
+	};
 	return (
-		<Accordion data-testid="TermsComponent">
-			<Table>
-				<thead>
-					<tr>
-						<th>Robota</th>
-						<th>Data rzeczywistego rozpoczęcia (DRR)</th>
-						<th>Data planowanego zakończenia (DPZ)</th>
-						<th>Data rzeczywistego zakończenia (DRZ)</th>
-					</tr>
-				</thead>
-				<tbody>
-					<Accordion.Toggle as="tr" eventKey="0">
-						{/* <tr> */}
-
-						<td>Jakaś długa nazwa roboty</td>
-
-						<td>asdasd</td>
-						<td>asdasdasdasd</td>
-						<td>asdasdasdasdasdasd</td>
-					</Accordion.Toggle>
-
-					<Accordion.Collapse eventKey="0">
-						<li>
-							<ul colSpan={3}>Hello! I'm the body</ul>
-							<ul>asdasd</ul>
-						</li>
-					</Accordion.Collapse>
-
-					{/* </tr> */}
-				</tbody>
-			</Table>
-		</Accordion>
+		<Table data-testid="TermsComponent">
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>Date</th>
+					<th>Description</th>
+					<th>Credit</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr onClick={onClickHandler}>
+					<td>1</td>
+					<td>05 May 2013</td>
+					<td className="text-success">$150.00</td>
+					<td className="text-success">$150.00</td>
+				</tr>
+				<tr className="collapse">
+					<td></td>
+					<td>Demo asddasasd</td>
+					<td>Demo asd</td>
+					<td>Demo asd</td>
+				</tr>
+				<tr onClick={onClickHandler}>
+					<td>2</td>
+					<td>05 May 2013</td>
+					<td className="text-success">$150.00</td>
+					<td className="text-success">$150.00</td>
+				</tr>
+				<tr className="collapse">
+					<td></td>
+					<td>Demo 22222</td>
+					<td>Demo as222d</td>
+					<td>Demo a22sd</td>
+				</tr>
+			</tbody>
+		</Table>
 	);
 }
 
