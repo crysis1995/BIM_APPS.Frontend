0f2871fc5be333fcd32d990de6e4b2f9ac757455
Krzysztof Kaczor <krzysztofkaczor95@gmail.com>
2021-03-31 11:44:11 +0200

    change date and rotation day visibility in tabs

--- src/sites/work_progress/components/Structural/Structural.Inputs.js
+++ src/sites/work_progress/components/Structural/Structural.Inputs.js
@@ -9,9 +9,15 @@ import {
 	selectDate,
 	selectRotationDate,
 } from '../../redux/actions/odbiory_actions';
+import { MONOLITHIC } from '../../redux/types/constans';
 import { parseDate } from '../../redux/utils/terms_utils';
 import Selector from '../../../../components/Selector';
 import { dateSelector } from './Structural.Inputs.Selector';
+
+function Hidden({ when, children }) {
+	if (!when) return children;
+	return null;
+}
 
 function Structural_Inputs({
 	cranes_loading,
@@ -21,6 +27,7 @@ function Structural_Inputs({
 	active_level,
 	rotation_day,
 	date,
+	active_tab,
 	//  actions
 	changeCrane,
 	changeLevel,
@@ -30,7 +37,7 @@ function Structural_Inputs({
 	selectRotationDate,
 }) {
 	return (
-		<div className="d-flex flex-row w-100">
+		<div className="d-flex flex-row w-100 pb-2">
 			<div className="w-25">
 				<Selector
 					classname={'mr-3'}
@@ -53,84 +60,91 @@ function Structural_Inputs({
 					onChangeValue={(id) => changeLevel(id)}
 				/>
 			</div>
-			<div className="w-50">
-				<div className="d-flex justify-content-between">
-					<Button
-						disabled={cranes_loading || !active_crane || !active_level}
-						size={'sm'}
-						onClick={() => decrementDay()}
-						variant={'secondary'}
-						className={'mb-3'}>
-						<svg
-							width="1em"
-							height="1em"
-							viewBox="0 0 16 16"
-							className="bi bi-chevron-double-left"
-							fill="currentColor"
-							xmlns="http://www.w3.org/2000/svg">
-							<path
-								fillRule="evenodd"
-								d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
-							/>
-							<path
-								fillRule="evenodd"
-								d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
-							/>
-						</svg>
-					</Button>
-					<div className="form-group ml-2 mr-1">
-						<label>Data rotacji</label>
-						<input
-							data-testid="data-input-1"
+			<Hidden when={active_tab === MONOLITHIC.TABS.DELAY}>
+				<div className="w-50">
+					<div className="d-flex justify-content-between">
+						<Button
 							disabled={cranes_loading || !active_crane || !active_level}
-							type={'date'}
-							className="form-control form-control-sm "
-							onChange={(selectedDay) => selectDate(selectedDay.target.value)}
-							value={parseDate(date)}
-						/>
-					</div>
-					<div className="form-group ml-1 mr-2">
-						<label>Dzień rotacji </label>
-						<input
+							size={'sm'}
+							onClick={() => decrementDay()}
+							variant={'secondary'}
+							className={'mb-3'}>
+							<svg
+								width="1em"
+								height="1em"
+								viewBox="0 0 16 16"
+								className="bi bi-chevron-double-left"
+								fill="currentColor"
+								xmlns="http://www.w3.org/2000/svg">
+								<path
+									fillRule="evenodd"
+									d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+								/>
+								<path
+									fillRule="evenodd"
+									d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+								/>
+							</svg>
+						</Button>
+						<Hidden when={active_tab === MONOLITHIC.TABS.ACTUAL}>
+							<div className="form-group ml-2 mr-1 w-100">
+								<label>Data rotacji</label>
+								<input
+									data-testid="data-input-1"
+									disabled={cranes_loading || !active_crane || !active_level}
+									type={'date'}
+									className="form-control form-control-sm "
+									onChange={(selectedDay) => selectDate(selectedDay.target.value)}
+									value={parseDate(date)}
+								/>
+							</div>
+						</Hidden>
+						<Hidden when={active_tab === MONOLITHIC.TABS.HISTORICAL}>
+							<div className="form-group ml-1 mr-2 w-100">
+								<label>Dzień rotacji </label>
+								<input
+									disabled={cranes_loading || !active_crane || !active_level}
+									data-testid="data-input-2"
+									type={'number'}
+									className="form-control form-control-sm "
+									onChange={(selectedDay) => selectRotationDate(parseInt(selectedDay.target.value))}
+									value={rotation_day}
+								/>
+							</div>
+						</Hidden>
+						<Button
 							disabled={cranes_loading || !active_crane || !active_level}
-							data-testid="data-input-2"
-							type={'number'}
-							className="form-control form-control-sm "
-							onChange={(selectedDay) => selectRotationDate(parseInt(selectedDay.target.value))}
-							value={rotation_day}
-						/>
+							size={'sm'}
+							onClick={() => incrementDay()}
+							className={'mb-3'}
+							variant={'secondary'}>
+							<svg
+								width="1em"
+								height="1em"
+								viewBox="0 0 16 16"
+								className="bi bi-chevron-double-right"
+								fill="currentColor"
+								xmlns="http://www.w3.org/2000/svg">
+								<path
+									fillRule="evenodd"
+									d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z"
+								/>
+								<path
+									fillRule="evenodd"
+									d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z"
+								/>
+							</svg>
+						</Button>
 					</div>
-					<Button
-						disabled={cranes_loading || !active_crane || !active_level}
-						size={'sm'}
-						onClick={() => incrementDay()}
-						className={'mb-3'}
-						variant={'secondary'}>
-						<svg
-							width="1em"
-							height="1em"
-							viewBox="0 0 16 16"
-							className="bi bi-chevron-double-right"
-							fill="currentColor"
-							xmlns="http://www.w3.org/2000/svg">
-							<path
-								fillRule="evenodd"
-								d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z"
-							/>
-							<path
-								fillRule="evenodd"
-								d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z"
-							/>
-						</svg>
-					</Button>
 				</div>
-			</div>
+			</Hidden>
 		</div>
 	);
 }
 
 const mapStateToProps = (state) => ({
 	cranes: state.Odbiory.OdbioryComponent.MONOLITHIC.cranes,
+	active_tab: state.Odbiory.OdbioryComponent.MONOLITHIC.active_tab,
 	active_crane: state.Odbiory.OdbioryComponent.MONOLITHIC.active_crane,
 	levels: state.Odbiory.OdbioryComponent.MONOLITHIC.levels,
 	active_level: state.Odbiory.OdbioryComponent.MONOLITHIC.active_level,
