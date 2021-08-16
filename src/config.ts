d57667ec0cc26d89cfda245aa42e399040bbd080
Krzysztof Kaczor <krzysztofkaczor95@gmail.com>
2021-08-16 10:06:52 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,48 +2,12 @@ const DEV_URI = 'http://localhost:2000';
 const PROD_URI = 'http://bim.warbud.pl';
 
 export const config = {
-	// api_url: "http://localhost:2000",
 	api_url: PROD_URI,
 	bim_apps_api: {
 		url: process.env.NODE_ENV === 'development' ? DEV_URI : PROD_URI + '/bim_apps/api',
 		graphql: process.env.NODE_ENV === 'development' ? DEV_URI + '/graphql' : PROD_URI + '/bim_apps/api/graphql',
 	},
-	// units: {
-	// 	area: {
-	// 		color_map: {
-	// 			1: { color: '#c9c9c9', option: '%=0', condition: (x: number) => x === 0 },
-	// 			2: {
-	// 				color: '#ff0000',
-	// 				option: '0>%>25',
-	// 				condition: (x: number) => x > 0 && x < 0.25,
-	// 			},
-	// 			3: {
-	// 				color: '#ffff00',
-	// 				option: '25>=%>50',
-	// 				condition: (x: number) => x >= 0.25 && x < 0.5,
-	// 			},
-	// 			4: {
-	// 				color: '#00b0fa',
-	// 				option: '50>=%>75',
-	// 				condition: (x: number) => x >= 0.5 && x < 0.75,
-	// 			},
-	// 			5: {
-	// 				color: '#6f30a0',
-	// 				option: '75>=%>100',
-	// 				condition: (x: number) => x >= 0.75 && x < 1,
-	// 			},
-	// 			6: { color: '#00B050', option: '%=100', condition: (x: number) => x === 1 },
-	// 		},
-	// 	},
-	// 	entity: {
-	// 		color_map: {
-	// 			1: { color: '#C9C9C9', option: 'Brak' },
-	// 			2: { color: '#FF0000', option: 'Nie rozpoczęte' },
-	// 			3: { color: '#FFFF00', option: 'W trakcie' },
-	// 			4: { color: '#00B050', option: 'Ukończone' },
-	// 		},
-	// 	},
-	// },
+
 	ACCEPTANCE: {
 		MONOLITHIC: {
 			legend: {
